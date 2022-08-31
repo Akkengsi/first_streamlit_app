bb1ae3260d9725b42887b0bda505ee79cbc6f789
Akkengsi <110946744+Akkengsi@users.noreply.github.com>
2022-08-31 23:45:41 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ streamlit.title('My Parents new Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text ('🐓Hard-Boiled Free-Range Egg')
 streamlit.text ('🥑Avocado Toast')
+streamlit.header ('🍌🥭Build Your Own Fruit Smoothie🥝🍓')
