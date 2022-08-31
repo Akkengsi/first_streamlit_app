ca34b3372d7e43e9aac63a67a0e81656af514093
Akkengsi <110946744+Akkengsi@users.noreply.github.com>
2022-08-31 23:36:39 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
+
 streamlit.title('My Parents new Healthy Dinner')
+
+steamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spninach & Rocket Smoothie')
+streamlit.text ('Hard-Boiled Free-Range Egg')
