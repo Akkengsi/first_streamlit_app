64ffa9628e7ff1f9a9ae229315358455ba2f0742
Akkengsi <110946744+Akkengsi@users.noreply.github.com>
2022-09-14 23:32:13 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,15 +19,16 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-import snowflake.connector
+
 # new section to display fruity api response
 streamlit.header('Fruity Fruit Advise!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-#import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
@@ -36,7 +37,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) ######
 streamlit.dataframe(fruityvice_normalized)
 #don't run anything past here while we troubleshoot
 streamlit.stop()
-
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
