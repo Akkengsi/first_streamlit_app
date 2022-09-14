05fccc15f20efbbeb5bb05c6cb4b97497bc9605e
Akkengsi <110946744+Akkengsi@users.noreply.github.com>
2022-09-14 22:34:42 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,27 +3,20 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-
 streamlit.title('My Parents new Healthy Dinner')
-
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text ('🐓Hard-Boiled Free-Range Egg')
 streamlit.text ('🥑Avocado Toast')
 streamlit.header ('🍌🥭Build Your Own Fruit Smoothie🥝🍓')
-
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
-
 # new section to display fruity api response
 streamlit.header('Fruity Fruit Advise!')
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
@@ -32,33 +25,23 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information")
-  else:
+   else:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
         streamlit.dataframe(fruityvice_normalized)
-        
 except URLError as e:
     streamlit.error()
-
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#streamlit.text(fruityvice_response.json())
+# write your own comment -what does the next line do? 
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+#streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
-
-
-
-streamlit.header("The fruit load list contains:")
-#snowflake -related functions
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
-         return my_cur.fetchall()
-        
- #add a button to load the fruit
-if streamlit.button ('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
-    my_data_rows = get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)
-
-
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-#this will not work correctly as per the link but go with it for now
-my_cur.execute("INSERT into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchall()
+#my_data_row = my_cur.fetchone()
